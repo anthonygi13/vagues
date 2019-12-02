14c975d8fa652f90f5b29e0f12f1a6685dba3d01
Anthony Giraudo <anthonygi13@hotmail.fr>
2019-12-02 19:37:11 +0100

    Première version du code avec animation mais bugs à arranger

--- animations.py
+++ animations.py
@@ -7,12 +7,54 @@
 """
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from main_functions import *
 
+
+# parametres du probleme (en unite SI)
 xmax = 0.5
 ymax = 0.5
 
-dt = 0.01
-dx = xmax
-ymax = 
+dt = 0.001
+dx = 0.001
+dy = 0.001
+
+h = 0.1
+g = 9.81
+c = np.sqrt(g*h)
+
+x = np.arange(0, xmax, dx)
+y = np.arange(0, ymax, dy)
+x, y = np.meshgrid(x, y)
+x, y = x.T, y.T
+
+# conditions initiales (onde plane)
+T = 0.1
+A = 0.025
+grille_t0 = h + A*np.cos(x*2*np.pi/T)
+grille_t1 = h + A*np.cos((x-c*dt)*2*np.pi/T)
+
+# plot
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+grille_tnm1 = grille_t0
+grille_tn = grille_t1
+
+ax.plot_surface(x, y, grille_t0)
+ax.set_zlim3d(0, h*2)
+plt.pause(dt)
+
+ax.clear()
+ax.plot_surface(x, y, grille_t1)
+ax.set_zlim3d(0, h*2)
+plt.pause(dt)
+
+# FIXME: faire qqchose pour la lenteur de l'animation et le bug des bords
+while plt.get_fignums():
+    grille_tn, grille_tnm1 = temps_suivant(grille_tn, grille_tnm1, dt, dx, dy, c), grille_tn
+    ax.clear()
+    ax.plot_surface(x, y, grille_tn)
+    ax.set_zlim3d(0, h * 2)
+    plt.pause(dt)
--- main_functions.py
+++ main_functions.py
@@ -8,9 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# TODO : fonctions qui renvoient des conditions initiales interessantes predeterminees (gaussienne, onde plane, etc.)
-
-
 def partial2_x(grille, dx):
     res = np.zeros(grille.shape)
     res[1:-1] = (grille[2:] - 2*grille[1:-1] + grille[:-2]) / dx**2
@@ -21,19 +18,18 @@ def partial2_x(grille, dx):
 
 def partial2_y(grille, dy):
     res = np.zeros(grille.shape)
-    res[:, 1:-1] = (grille[:, 2:] - 2 * grille[:, 1:-1] + grille[:, -2]) / dy**2
+    res[:, 1:-1] = (grille[:, 2:] - 2 * grille[:, 1:-1] + grille[:, :-2]) / dy**2
     res[:, 0] = res[:, 1]
     res[:, -1] = res[:, -2]
     return res
 
 
-def temps_suivant(grille_tn, grille_tnm1, dt, dx, dy, h):
+def temps_suivant(grille_tn, grille_tnm1, dt, dx, dy, c):
     """
     :param grille_tnm1: configuration au temps t_(n-1)
     :param grille_tnm2: configuration au temps t_(n-2)
     :param dt: pas de temps
     :return: configuration au temps t_n
     """
-    return dt**2 * g*h * (partial2_x(grille_tn, dx) + partial2_y(grille_tn, dy)) + 2*grille_tn - grille_tnm1
-
+    return dt**2 * c**2 * (partial2_x(grille_tn, dx) + partial2_y(grille_tn, dy)) + 2*grille_tn - grille_tnm1
 
