1c9b0adc9957c71923bb10e2744c7a958d8a9a59
Anthony Giraudo <anthonygi13@hotmail.fr>
2019-12-13 11:15:32 +0100

    Version finale du code

--- animations.py
+++ animations.py
@@ -3,7 +3,7 @@
 
 # File : animations.py
 
-"""
+"""Script qui fait la modelisation et cree les animations
 """
 
 import matplotlib.pyplot as plt
@@ -13,19 +13,65 @@ import matplotlib.animation as animation
 from main_functions import *
 
 
-def animate(frame):
+def conditions_initiales1(nb_periodes=5, A=0.025):
+    """
+    Conditions initiales pour onde plane
+    :param nb_periodes: nombre de periodes spatiales dessinees
+    :param A: amplitude de l'onde
+    :return: grilles initiales aux temps t0 et t1
+    """
+    check_nb_periodes(nb_periodes)
+    T = xmax / nb_periodes
+    grille_t0 = h + A * np.cos(x * 2 * np.pi / T)
+    grille_t1 = h + A * np.cos((x - c * dt) * 2 * np.pi / T)
+    return grille_t0, grille_t1
+
+
+def conditions_initiales2(nb_periodes_x=2, nb_periodes_y=2, A=0.025):
+    """
+    Conditions initiales pour une onde interessante visuellement
+    :param nb_periodes_x: nombre de periodes spatiales dessinees pour l'onde se propageant suivant x
+    :param nb_periodes_y: nombre de periodes spatiales dessinees pour les ondes se propageant suivant y
+    :param A: amplitude caracteristique de l'onde
+    :return: grilles initiales aux temps t0 et t1
+    """
+    check_nb_periodes(nb_periodes_x)
+    check_nb_periodes(nb_periodes_y)
+    Tx = xmax / nb_periodes_x
+    Ty = ymax / nb_periodes_y
+    grille_t0 = h + A*np.cos(x*2*np.pi/Tx) + A/2*np.cos(y*2*np.pi/Ty + np.pi/2) + A/2*np.cos(y*2*np.pi/Ty + np.pi/2)
+    grille_t1 = h + A*np.cos((x-c*dt)*2*np.pi/Tx) + A/2*np.cos((y-c*dt)*2*np.pi/Ty + np.pi/2) + A/2*np.cos((y+c*dt)*2*np.pi/Ty + np.pi/2)
+    return grille_t0, grille_t1
+
+
+def check_nb_periodes(nb_periodes):
+    """
+    Verifie que nb_periode soit un entier pour qu'on puisse mettre des conditions au bord periodiques
+    :param nb_periodes: un nombre de periodes spatiales dessinees
+    """
+    if type(nb_periodes) != int:
+        raise TypeError("Le nombre de periodes nb_periodes doit etre un entier (pour la periodisation).")
+
+
+def animate(frame, animation_id):
+    """
+    Fonction appelee pour la creation de chaque frame de l'animation
+    :param frame: numero de la frame
+    :param animation_id: numero de l'animation
+    """
     global grille_tn
     global grille_tnm1
     grille_tn, grille_tnm1 = temps_suivant(grille_tn, grille_tnm1, dt, dx, dy, c), grille_tn
     plot[0].remove()
     plot[0] = ax.plot_surface(x, y, grille_tn, color='blue')
-    print("{}% done".format(int(frame/nframe*100)))
+    print("Animation {}/{}: {}% done".format(animation_id+1, len(conditions_initiales), int(frame/nframe*100)))
+
 
 # parametres du probleme (en unite SI)
 xmax = 0.5
 ymax = 0.5
 
-dt = 0.001
+dt = 0.0005
 dx = 0.001
 dy = 0.001
 
@@ -33,31 +79,26 @@ h = 0.1
 g = 9.81
 c = np.sqrt(g*h)
 
+
+# settings
 x = np.arange(0, xmax, dx)
 y = np.arange(0, ymax, dy)
 x, y = np.meshgrid(x, y)
 x, y = x.T, y.T
 
-# conditions initiales (onde plane)
-nb_periodes = 5  # entier
-T = xmax / nb_periodes
-A = 0.025
-grille_t0 = h + A*np.cos(x*2*np.pi/T)
-grille_t1 = h + A*np.cos((x-c*dt)*2*np.pi/T)
 
-# plot
-nframe = 100
+# animations
+nframe = 300
+conditions_initiales = (conditions_initiales1, conditions_initiales2)
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+for i, condition_initiale in enumerate(conditions_initiales):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
 
-grille_tnm1 = grille_t0
-grille_tn = grille_t1
+    grille_tnm1, grille_tn = condition_initiale()
 
-plot = [ax.plot_surface(x, y, grille_t0, color='blue')]
-ax.set_zlim(0, h*2)
+    plot = [ax.plot_surface(x, y, grille_tnm1, color='blue')]
+    ax.set_zlim(0, h*2)
 
-# TODO: afficher animation ?
-
-ani = animation.FuncAnimation(fig, animate, frames=nframe, interval=50, repeat=True)
-ani.save('wave.mp4')
+    ani = animation.FuncAnimation(fig, animate, fargs=(i,), frames=nframe, interval=25, repeat=True)
+    ani.save('wave{}.mp4'.format(i+1))
--- main_functions.py
+++ main_functions.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# File : functions.py
+# File : main_functions.py
 
+"""Fonctions qui calcule l'evolution de la grille des hauteurs de la vague
+"""
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 
 def partial2_x(grille, dx):
-    grille1back = np.roll(grille, -1, axis=1)
-    grille1forward = np.roll(grille, 1, axis=1)
-    res = (grille1forward - 2*grille + grille1back) / dx**2
+    """
+    :param grille: grille des hauteurs
+    :param dx: pas de discretisation suivant les x
+    :return: grille des derivees secondes de la hauteur par rapport a x
+    """
+    grilleBackward = np.roll(grille, -1, axis=1)
+    grilleForward = np.roll(grille, 1, axis=1)
+    res = (grilleForward - 2*grille + grilleBackward) / dx**2  # conditions aux bords periodiques
     return res
 
 
 def partial2_y(grille, dy):
-    grille1back = np.roll(grille, -1, axis=0)
-    grille1forward = np.roll(grille, 1, axis=0)
-    res = (grille1forward - 2*grille + grille1back) / dy**2
+    """
+    :param grille: grille des hauteurs
+    :param dx: pas de discretisation suivant les y
+    :return: grille des derivees secondes de la hauteur par rapport a y
+    """
+    grilleBackward = np.roll(grille, -1, axis=0)
+    grilleForward = np.roll(grille, 1, axis=0)
+    res = (grilleForward - 2*grille + grilleBackward) / dy**2  # conditions aux bords periodiques
     return res
 
 
@@ -26,7 +37,7 @@ def temps_suivant(grille_tn, grille_tnm1, dt, dx, dy, c):
     """
     :param grille_tnm1: configuration au temps t_(n-1)
     :param grille_tnm2: configuration au temps t_(n-2)
-    :param dt: pas de temps
+    :param dt: pas de discretisation du temps
     :return: configuration au temps t_n
     """
     return dt**2 * c**2 * (partial2_x(grille_tn, dx) + partial2_y(grille_tn, dy)) + 2*grille_tn - grille_tnm1
